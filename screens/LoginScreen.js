74a008bcc5344758517af2972b845146f01739bd
AyeshaAreej <ayeshaareej222@gmail.com>
2022-04-19 02:16:04 +0500

    LoginScreen in process

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,23 +1,41 @@
 import React from "react";
-import { ImageBackground, Platform, ScrollView, StyleSheet, View, Image, Text } from "react-native";
+import { ImageBackground, Icon, TextInput, Platform,ScrollView, StyleSheet, View, Image, Text } from "react-native";
 import { StatusBar } from "react-native-web";
 import { Colors } from "../components/styles";
+import {Icons} from 'react-native-vector-icons'
 function LoginScreen() {
   return (
+    
    <View style={{flex:1, backgroundColor:'#fff'}} >
     <ImageBackground  
-    source={require('../assets/logo1.png')}
-     resizeMode="cover" style={styles.image} >
+    
+     source={require('../assets/logo1.png')}
+     resizeMode="cover" style={styles.image } >
          <Text style={styles.tagline}>
          Blue Jay Event Planners
          </Text>
          
-        
-    </ImageBackground>
-    <View style={styles.bottomView}>
-      {/* Welcome view */}
-        <View>
+     </ImageBackground>
 
+      {/*Bottom    view */}
+     <View style={styles.bottomView}>
+      {/* Welcome View */}
+        <View style={{padding:20}}>
+        <Text style={{color:'#4632A1', fontSize:34}}>Welcome</Text>
+        <Text>Don't have an  account?
+        <Text style={{color:'red', fontStyle:'italic '}}>
+          {''}
+         Register now</Text>
+        </Text>
+        {/* Form Inputs View */}
+          <View style={{marginTop:50}}>
+            
+
+             <TextInput placeholder="Email"
+      />
+            
+           
+          </View>
         </View>
     </View>
    
@@ -29,10 +47,12 @@ const styles = StyleSheet.create({
   
   image: {
     alignItems:"center",
+    backgroundColor:"#EFE4B0",
     justifyContent:"center",
-     height: '50%',
+     height: '50%',     
     paddingTop: Platform.OS==='android' ? StatusBar.CurrentHeight :0,
-    
+    borderBottomStartRadius:60,
+    borderBottomEndRadius:60, 
   },
  brandView :{
   flex: 1,
@@ -41,12 +61,9 @@ const styles = StyleSheet.create({
  },
   bottomView:{
   flex:1,
-  backgroundColor:'pink',
-  
-  borderTopStartRadius:60,
-  borderTopEndRadius:60,
-
-  },
+  backgroundColor:Colors.primary,
+  backgroundColor :"#fff"
+ },
 
   buttonContainer:{
  padding:20,
@@ -61,10 +78,11 @@ const styles = StyleSheet.create({
    tagline:{
     position: "absolute",
     paddingTop:"25%",
-   fontSize:25,
-   fontWeight:"600",
-   paddingVertical:20,
-  color:Colors.darkLight,
+    fontSize:23,
+    fontWeight:"bold",
+    textTransform:"uppercase",
+    paddingVertical:20,
+    color:'#D9AEA5',
    },
 
 });
