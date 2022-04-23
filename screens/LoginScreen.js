0a2148f20230c30d1a19eec7cccb73f1075caa13
AyeshaAreej <ayeshaareej222@gmail.com>
2022-04-23 22:34:08 +0500

    SignIn Screen Modified

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -18,29 +18,19 @@ function LoginScreen() {
 
   return (
     
-   <View style={{flex:1, backgroundColor:'#EFE4B0'}} >
-    <ImageBackground  
+   <View style={{flex:1}} >
+   <View style={styles.topView}>
+   <ImageBackground
+   style={styles.logo}
+     source={require('../assets/logo2.1.png')}
+     resizeMode="cover" >
+    </ImageBackground>
+     <Text style={{color:'white', fontSize:25,fontWeight:'bold'}}>Sign In</Text>
     
-     source={require('../assets/logo1.png')}
-     resizeMode="cover" style={styles.image } >
-         <Text style={styles.tagline}>
-         Blue Jay Event Planners
-         </Text>
-         
-     </ImageBackground>
+   </View>
 
-      {/*Bottom    view */}
-     <View style={styles.bottomView}>
-      {/* Welcome View */}
-        <View style={{padding:20}}>
-        <Text style={{color:'#4632A1', fontSize:34}}>Welcome</Text>
-        <Text>Don't have an  account?
-        <Text style={{color:'red', fontStyle:'normal'}}>
-          {''}
-         Register now</Text>
-        </Text>
-        {/* Form Inputs View */}
-          <View style={{marginTop:50}}>
+      {/* Form Inputs View */}
+      <View style={{marginTop:50}}>
           <Formik
       initialValues={{ email: '', password: '' }}
       onSubmit={values => console.log(values)}
@@ -85,53 +75,40 @@ function LoginScreen() {
             
            
           </View>
-        </View>
-    </View>
-   
+  
+    
+
+    
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   
-  image: {
-    alignItems:"center",
-    backgroundColor:"#EFE4B0",
-    justifyContent:"center",
-     height: '50%',     
-    paddingTop: Platform.OS==='android' ? StatusBar.CurrentHeight :0,
-    borderBottomStartRadius:60,
-    borderBottomEndRadius:60, 
-  },
- brandView :{
-  flex: 1,
-  justifyContent: "center",
-  alignItems: "center",
- },
-  bottomView:{
-  flex:1,
-  backgroundColor:Colors.primary,
-  backgroundColor :"#fff",
-  borderTopStartRadius:60,
-  borderTopEndRadius:60, 
- },
-
-   tagline:{
-    position: "absolute",
-    paddingTop:"25%",
-    fontSize:23,
-    fontWeight:"bold",
-    textTransform:"uppercase",
-    paddingVertical:20,
-    color:'#D9AEA5',
+  topView:{
+    backgroundColor :"#9370DB",
+    paddingTop:"18%",
+    paddingLeft:8,
+    width:360,
+    height:220,
+    //  borderTopStartRadius:60,
+    //  borderTopEndRadius:60, 
    },
+
+ logo: {
+  width: 200,
+  height: 110,
+  marginLeft:70,
+ 
+
+},
+   
    input:{
-   borderBottomColor :'#D291BC',
-  borderBottomWidth:1,
-   padding:6,
-   paddingLeft:20,
-   marginLeft:50,
-   margin:12,
+   borderBottomColor :'#9370DB',
+   borderBottomWidth:1,
+   padding:8,
+   marginLeft:80,
+   margin:20,
    width:200,
 
    },
