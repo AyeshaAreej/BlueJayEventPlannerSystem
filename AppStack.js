299fc6f2df2dad137d7d21616b4075c00793a31b
Ume Aemun <81216378+umeaemun@users.noreply.github.com>
2022-04-26 02:05:26 +0500

    drawer done

--- AppStack.js
+++ AppStack.js
@@ -7,9 +7,11 @@ import Location from './screens/Location';
 import Message from './screens/Message';
 import HelpCentre from './screens/HelpCentre';
 import PaymentMethod from './screens/PaymentMethod';
+import Notifications from './screens/Notifications'
 
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import CustomDrawer from './components/CustomDrawer';
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 
 const Drawer = createDrawerNavigator();
@@ -17,15 +19,104 @@ const Drawer = createDrawerNavigator();
 
 const AppStack = () => {
   return (
-    <Drawer.Navigator drawerContent={props=><CustomDrawer {...props} /> } useLegacyImplementation>
-        <Drawer.Screen name='HomeScreen' component={HomeScreen}/>
-    <Drawer.Screen name='profile' component={Profile} />
-     <Drawer.Screen name='MyOrders' component={MyOrders} /> 
-     <Drawer.Screen name='Location' component={Location} />
-     <Drawer.Screen name='Message' component={Message} />
-     <Drawer.Screen name='HelpCentre' component={HelpCentre} />
-     <Drawer.Screen name='PaymentMethod' component={PaymentMethod} />
-</Drawer.Navigator>
+    <Drawer.Navigator 
+    drawerContent={props=><CustomDrawer {...props} /> } 
+    screenOptions={{
+      drawerActiveBackgroundColor:'purple',
+      drawerActiveTintColor:'white',
+      drawerInactiveTintColor:'#333'
+    }}
+    useLegacyImplementation>
+          <Drawer.Screen name='Home' component={HomeScreen} 
+            options={{
+              drawerIcon:({focused,size})=>(
+                <Icon
+                name='home'
+                size={size}
+                color={focused? 'white' : 'purple'}
+                />
+              ),
+            }}
+            
+          />
+          <Drawer.Screen name=' Profile' component={Profile}
+              options={{
+                drawerIcon:({focused,size})=>(
+                  <Icon
+                  name='user'
+                  size={size}
+                  color={focused? 'white' : 'purple'}
+                  />
+                ),
+              }}
+          />
+          <Drawer.Screen name='My Orders' component={MyOrders} 
+              options={{
+                drawerIcon:({focused,size})=>(
+                  <Icon
+                  name='shopping-cart'
+                  size={size}
+                  color={focused? 'white' : 'purple'}
+                  />
+                ),
+              }}
+          /> 
+          <Drawer.Screen name='Notifications' component={Notifications} 
+                  options={{
+                    drawerIcon:({focused,size})=>(
+                      <Icon
+                      name='bell'
+                      size={size}
+                      color={focused? 'white' : 'purple'}
+                      />
+                    ),
+                  }}
+                  />
+          <Drawer.Screen name='   Location' component={Location} 
+              options={{
+                drawerIcon:({focused,size})=>(
+                  <Icon
+                  name='map-marker'
+                  size={size}
+                  color={focused? 'white' : 'purple'}
+                  />
+                ),
+              }}
+          />
+          <Drawer.Screen name=' Message' component={Message}
+                options={{
+                  drawerIcon:({focused,size})=>(
+                    <Icon
+                    name='comment'
+                    size={size}
+                    color={focused? 'white' : 'purple'}
+                    />
+                  ),
+                }}
+          />
+          <Drawer.Screen name='Payment' component={PaymentMethod} 
+                  options={{
+                    drawerIcon:({focused,size})=>(
+                      <Icon
+                      name='cc-visa'
+                      size={size}
+                      color={focused? 'white' : 'purple'}
+                      />
+                    ),
+                  }}
+          />
+          <Drawer.Screen name='  Help Centre' component={HelpCentre}
+                  options={{
+                    drawerIcon:({focused,size})=>(
+                      <Icon
+                      name='question-circle'
+                      size={size}
+                      color={focused? 'white' : 'purple'}
+                      />
+                    ),
+                  }}
+           />
+    </Drawer.Navigator>
   )
 }
 
--- components/CustomDrawer.js
+++ components/CustomDrawer.js
@@ -1,34 +1,48 @@
-import { View, Text, ImageBackground,Image} from 'react-native'
+import { View, Text, ImageBackground,Image, TouchableOpacity} from 'react-native'
 import React from 'react'
 import { DrawerContentScrollView, DrawerItemList} from '@react-navigation/drawer'
+import { DrawerActions } from '@react-navigation/native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 
 const CustomDrawer = (props) => {
   return (
-      <view style={{flex:1}}>
+      <View style={{flex:1}}>
 
-        <DrawerContentScrollView 
-        {...props} 
-        contentContainerStyle={{backgroundColor: 'purple'}}>
-           
-            <ImageBackground source={require('../assets/bg.jpg')} style={{padding:20}}>
-                <Image source={require('../assets/profile.jpg')} 
-                style={{height:80, width:80,borderRadius:40, marginBottom:10}}/>
-                <Text style={{color:'#fff',fontSize:18,fontFamily:'Roboto-Medium'}}>Ayesha Areej</Text>
-            </ImageBackground>
-
-            <view style={{flex:1,backgroundColor:'#fff',paddingTop: 10}}>
-                <DrawerItemList {...props} />
-            </view>
+                <DrawerContentScrollView {...props} contentContainerStyle={{backgroundColor:'black'}}>
                 
+                            <ImageBackground source={require('../assets/bg.jpg')} style={{padding:20}}>
+                                <Image source={require('../assets/profile.jpg')} 
+                                style={{height:80, width:80,borderRadius:40, marginBottom:10}}/>
+                                <Text style={{color:'#fff',fontSize:18,fontFamily:'Roboto-Medium'}}>Ayesha Areej</Text>
+                            </ImageBackground>
 
-         </DrawerContentScrollView>
+                            <View style={{flex:1, backgroundColor:'#fff', paddingTop: 10}}>
+                                <DrawerItemList {...props} />
+                            </View>
 
-         <view>
-             <Text>Our custom text</Text>
-         </view>
+                </DrawerContentScrollView>
 
-      </view>   
+                <View style={{padding:20, borderTopWidth:1, borderTopColor:'#ccc' }}>
+
+                    <TouchableOpacity onPress={()=>{}} style={{paddingVertical:15}}>
+                    <View style={{flexDirection:'row',alignItems:'center'}}>
+                         <Icon name='share-alt' size={22} color='grey'/>
+                          <Text>    Share with friends</Text> 
+                    </View>
+                    </TouchableOpacity>
+                    
+                    <TouchableOpacity onPress={()=>{}} style={{paddingVertical:15}}>
+                    <View style={{flexDirection:'row',alignItems:'center'}}>
+                         <Icon name='sign-out' size={22} color='grey'/>
+                          <Text>    Sign out</Text> 
+                    </View>
+                    </TouchableOpacity>
+                    
+                </View>
+
+      </View>   
+   
   )
 }
 
