caa04e9fb831e4af352ec2d3d056aed2b4d07385
AyeshaAreej <ayeshaareej222@gmail.com>
2022-08-05 21:37:25 +0500

    Notification screen designing done

--- FrontEnd/screens/Notifications.js
+++ FrontEnd/screens/Notifications.js
@@ -1,21 +1,68 @@
-import { View, Text, StyleSheet, Platform, Button } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity,Image, Platform, Button, ScrollView } from 'react-native'
 import React from 'react'
+import colors from '.././components/colors'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import {useState, useEffect,useRef, useReducer}  from 'react'
 // import * as Notificationss from 'expo-notifications';
 // import * as Device from 'expo-device';
 
-
-
 const Notifications = () => {
-
- 
 
   
   return (
-    <View>
-    <Text>Notification</Text>
-  </View>
+    <ScrollView style={{flex:1, backgroundColor:colors.white,}} contentContainerStyle={{justifyContent:'center', alignItems:'center'}}>
+    
+    <View  style={styles.notificationContainer}> 
+      <Image source={require('.././assets/profile.jpg')} style={styles.profileImage} />
+      <View style={{ margin:20, }}>
+       <Text style={{  fontSize: 23,  color: colors.primary,fontStyle:'italic', marginBottom:10   }}>Floral Weddings Banquet</Text>
+       <Text style={{  fontSize: 20,  color: colors.dark,   }}>Your Order has been accepted. </Text>
+      </View>
+   </View>
+
+   <View  style={styles.notificationContainer}> 
+      <Image source={require('.././assets/profile.jpg')} style={styles.profileImage} />
+      <View style={{ margin:20, }}>
+       <Text style={{  fontSize: 23,  color: colors.primary,fontStyle:'italic', marginBottom:10   }}>Hot & Spicy Foods</Text>
+       <Text style={{  fontSize: 20,  color: colors.dark,   }}>Your Order has been rejected. </Text>
+      </View>
+   </View>
+
+   <View  style={styles.notificationContainer}> 
+      <Image source={require('.././assets/profile.jpg')} style={styles.profileImage} />
+      <View style={{ margin:20, }}>
+       <Text style={{  fontSize: 23,  color: colors.primary,fontStyle:'italic', marginBottom:10   }}>Ilyana</Text>
+       <Text style={{  fontSize: 20,  color: colors.dark,   }}>Ordered for birthday party. </Text>
+      </View>
+   </View>
+
+
+   
+     
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+notificationContainer: {
+   flexDirection: 'row',
+   alignItems: 'center',
+   height:120,
+   marginTop:10,
+   width:'98%',
+   borderColor :colors.white,
+   elevation:20,
+   borderRadius:25,
+   backgroundColor:colors.grey
+   },
+ 
+ 
+  profileImage:{
+  height: 100,
+    width: '20%',
+    borderRadius: 30,
+
+ },
+ });
 
 export default Notifications
