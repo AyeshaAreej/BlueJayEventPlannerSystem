92f16e74b1d60a42272f2e092e570aa1ed1e56ad
Ume Aemun <81216378+umeaemun@users.noreply.github.com>
2022-10-08 23:34:10 +0500

    changed index

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -10,6 +10,8 @@ const User = require('./models/userSchema')
 const Company = require("./models/companySchema")
 const Vendor = require('./models/vendorSchema')
 const cors = require("cors")
+const Stripe = require('stripe')
+
 
 require('dotenv').config();
 const corsOptions = {
@@ -19,6 +21,12 @@ const corsOptions = {
 }
 
 const app = express()
+
+const PUBLISHABLE_KEY = process.env.PUBLISHABLE_KEY || 'pk_test_51LqdnhBKftHehnLjj3UjXBLpyXNDbAICMGFu9qyyhAFSvlWVhBW5MaV4lklaWM5QN9gRvEqL6z123cGZARCrqk7X00IuBzYM05'
+const SECRET_KEY = process.env.SECRET_KEY || 'sk_test_51LqdnhBKftHehnLjF88bzmAGPu2tox5RwMLcxdmSBJ9zNqwY0WugAV35LBgzmowVHWGrL1jyGR8TfQ3I09LFqJm300ui7S4gyw'
+
+
+const stripe = Stripe(SECRET_KEY, { apiVersion: "2022-08-01" });
 
 app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({extended : true}))
@@ -69,6 +77,25 @@ app.get('/getUser',middleware.ValidateToken,async (req,res)=>{
     }
 }
 )
+
+app.post("/create-payment-intent", async (req, res) => {
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: 1099, //lowest denomination of particular currency
+        currency: "usd",
+        payment_method_types: ["card"], //by default
+      });
+  
+      const clientSecret = paymentIntent.client_secret;
+  
+      res.json({ clientSecret: clientSecret, });
+
+    } catch (e) {
+      console.log(e.message);
+      res.json({ error: e.message });
+    }
+  })
+
 
 
 
--- FrontEnd/screens/PaymentMethod.js
+++ FrontEnd/screens/PaymentMethod.js
@@ -1,12 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import StripeApp from './StripeApp'
+import {StripeProvider} from '@stripe/stripe-react-native'
+
 
 const PaymentMethod = () => {
   return (
     <View>
-      <Text>PaymentMethod</Text>
-      <StripeApp/>
+
+      <StripeProvider publishableKey='pk_test_51LqdnhBKftHehnLjj3UjXBLpyXNDbAICMGFu9qyyhAFSvlWVhBW5MaV4lklaWM5QN9gRvEqL6z123cGZARCrqk7X00IuBzYM05'>
+        <StripeApp/>
+      </StripeProvider>
+
     </View>
   )
 }
