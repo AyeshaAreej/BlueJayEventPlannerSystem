20571b61d4beafdcf37833346a6b3b29974d0520
Ume Aemun <81216378+umeaemun@users.noreply.github.com>
2022-10-08 20:07:00 +0500

    updated screens

--- FrontEnd/screens/PaymentMethod.js
+++ FrontEnd/screens/PaymentMethod.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import StripeApp from './StripeApp'
 
 const PaymentMethod = () => {
   return (
     <View>
       <Text>PaymentMethod</Text>
+      <StripeApp/>
     </View>
   )
 }
---
+++ FrontEnd/screens/StripeApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const StripeApp= () => {
+    return (
+        <View>
+            <Text>StripeApp</Text>
+        </View>
+    )
+}
+
+export default StripeApp
